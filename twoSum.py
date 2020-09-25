8de22a38d716d3169be8cce7a44638d53f758486
Accepteda <924913496@qq.com>
2020-09-25 17:33:37 +0800

    Dubang

---
+++ twoSum.py
@@ -0,0 +1,13 @@
+    # 暴力
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for i in range(len(nums)):
+            for j in range(i+1. len(nums)):
+                if nums[i] + nums[j] == target:
+                return [i, j]
+        return []
+
+# 排序 + 双指针
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        temp
